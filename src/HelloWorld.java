28abae044adc7c934be355ddfd14ad7b7165af1d
Ja <Ja@158.197.239.41>
2018-09-19 08:04:18 +0200

    dolezity koment
    pridal som dolezity komentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@
 public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello world");
+		
+		//toto je dolezity komentar
 		for (int i=0;i<10;i++) {
 			System.out.println("Git je super");
 		}
