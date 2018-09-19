74bdc8580aba2b9b887b88741d435bbe2dd3d34d
Ja <Ja@158.197.239.41>
2018-09-19 07:32:52 +0200

    10x git je super
    pridany for cyklus vypisuje git je super

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,16 @@
 public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello world");
+		for (int i=0;i<10;i++) {
+			System.out.println("Git je super");
+		}
 	}
 
 }
+
+
+/*  working dir
+ * local prepository- lokalny repozitar na ukladanie vlastne verzie... potrebujeme vytvorit ... prave na projekt+team+ share project... 
+ * valec na projekte- znamena ze to je zdielany subor
+ * remote repository - server kde sa posielaju data z localneho repositaru
+ */
